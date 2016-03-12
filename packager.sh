025fc58846aa9c83294e33ce3aaefbffcea52870
Think Wu <yingxinwu.g@gmail.com>
2016-03-12 14:16:28 +0800

    fix readlink command error on OS X

--- packager.sh
+++ packager.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 RNMK_DIR="node_modules/react-native-material-kit"
 
+case `uname` in
+  Linux )
+    CMD_READLINK="readlink -f"
+    ;;
+  * )
+    CMD_READLINK="readlink"
+    ;;
+esac
+
 solveRNMK() {
   DIR="$1"
 
   if [[ -L "$DIR" ]];
   then
-    RNMK_PATH=`readlink -f $DIR`
+    RNMK_PATH=`$CMD_READLINK $DIR`
     if [[ -L "$RNMK_PATH" ]];
     then
       solveRNMK "$RNMK_PATH"
