83b7ba9870dac8a083dfe85aac0f2e5bed15d077
Think Wu <yingxinwu.g@gmail.com>
2020-02-15 09:10:33 +0800

    upgrade react-native & react-navigation

--- app/index.js
+++ app/index.js
@@ -6,7 +6,8 @@
 
 import React from 'react';
 import { StyleSheet, Text, ScrollView, TouchableOpacity, Platform } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 // import { setTheme, MKColor } from 'react-native-material-kit';
 
@@ -26,46 +27,46 @@ import Cards from './cards';
 
 const Home = props => (
   <ScrollView style={styles.list} contentContainerStyle={styles.container}>
-    <TouchableOpacity onPress={() => navigate(props, 'buttons')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Buttons')}>
       <Text style={styles.pushLabel}>Buttons</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate(props, 'cards')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Cards')}>
       <Text style={styles.pushLabel}>Cards</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate(props, 'progress')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Progress')}>
       <Text style={styles.pushLabel}>Loading</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate(props, 'sliders')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Sliders')}>
       <Text style={styles.pushLabel}>Sliders</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate(props, 'textfields')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Textfields')}>
       <Text style={styles.pushLabel}>Text Fields</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => navigate(props, 'toggles')}>
+    <TouchableOpacity onPress={() => navigate(props, 'Toggles')}>
       <Text style={styles.pushLabel}>Toggles</Text>
     </TouchableOpacity>
   </ScrollView>
 );
 
-Home.navigationOptions = {
-  title: 'Examples',
-};
-
-const MainNavigator = createStackNavigator({
-  home: { screen: Home },
-  buttons: { screen: Buttons },
-  cards: { screen: Cards },
-  progress: { screen: Progress },
-  sliders: { screen: Sliders },
-  toggles: { screen: Toggles },
-  textfields: { screen: TextFields },
-});
+const Stack = createStackNavigator();
 
 function navigate(props, route, params, action) {
   props.navigation.navigate(route, params, action);
 }
 
-const App = createAppContainer(MainNavigator);
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={Home} options={{ title: 'Examples' }} />
+      <Stack.Screen name="Buttons" component={Buttons} options={{ title: 'Buttons' }} />
+      <Stack.Screen name="Cards" component={Cards} options={{ title: 'Cards' }} />
+      <Stack.Screen name="Progress" component={Progress} options={{ title: 'Progress' }} />
+      <Stack.Screen name="Sliders" component={Sliders} options={{ title: 'Sliders' }} />
+      <Stack.Screen name="Toggles" component={Toggles} options={{ title: 'Toggles' }} />
+      <Stack.Screen name="Textfields" component={TextFields} options={{ title: 'Textfields' }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 const styles = StyleSheet.create({
   list: {
